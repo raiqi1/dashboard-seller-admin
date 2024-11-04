65dc53addd6dd8b34d66f9782263dd5bc4dbe534
rayiqi <ramadifan769@gmail.com>
2024-11-05 05:26:31 +0700

    ok

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -7,4 +7,9 @@ export const overrideStyle = {
     alignItems: "center"
 }
 
-export const socket = io('https://ecom-api-production-9723.up.railway.app')
+export const socket = io('https://ecom-api-production-9723.up.railway.app', {
+    withCredentials: true,
+    extraHeaders: {
+        "Access-Control-Allow-Origin": "https://dashboard-seller-admin.vercel.app"
+    }
+});
